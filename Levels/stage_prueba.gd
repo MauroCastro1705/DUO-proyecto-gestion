159fbb35c2913a61e33c1c26785a300c18665d4d
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2025-04-02 17:19:59 -0300

    character mechanics

--- Levels/stage_prueba.gd
+++ Levels/stage_prueba.gd
@@ -3,14 +3,17 @@ extends Node2D
 var active_player: CharacterBody2D  # El personaje que actualmente controlas
 var player1: CharacterBody2D
 var player2: CharacterBody2D
-
+@onready var posicionPuertaInicial = %puerta.global_position.y
+var posicionPuertaFinal = -102
+var is_moved: bool = false
 func _ready() -> void:
 	# Referencias a los personajes
 	player1 = %Gordo
-	player2 = %Flaco
+	player2 = %Flaco	
 	active_player = player1
 	player1.is_active = true
 	player2.is_active = false  # Inicia desactivado
+	
 
 
 func _input(event: InputEvent) -> void:
@@ -26,3 +29,12 @@ func swap_characters() -> void:
 		active_player = player1
 		player1.is_active = true
 		player2.is_active = false
+
+
+func _on_boton_boton_pulsado() -> void:		
+	if is_moved:
+		%puerta.global_position.y = posicionPuertaInicial
+		is_moved = false
+	else:
+		%puerta.global_position.y = posicionPuertaFinal
+		is_moved=true
