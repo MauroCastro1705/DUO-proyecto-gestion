91137e9544634ffbdc7a625fe0fec67553b44ab9
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2025-04-07 20:19:18 -0300

    addons de menues

--- Levels/stage_prueba.gd
+++ Levels/stage_prueba.gd
@@ -22,6 +22,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("cambiar"):
 		swap_characters()
+		
 
 func swap_characters() -> void:
 	if active_player == player1:
