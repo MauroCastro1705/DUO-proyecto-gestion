721f791d4c80c28fe33339b43a327508d03e113b
Mauro Castro <maurocastruli@gmail.com>
2025-05-21 16:10:48 -0300

    level transition update

--- Levels/niveles-finales/niveles-finales-codigo/level_1.gd
+++ Levels/niveles-finales/niveles-finales-codigo/level_1.gd
@@ -4,5 +4,9 @@ extends Node2D
 
 func _on_cambio_de_nivel_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador") and proximo_nivel:
-		var main = get_tree().current_scene
-		main.load_level(proximo_nivel) #metodo de main para cargar proximo nivel
+		call_deferred("_cambiar_nivel")
+
+
+func _cambiar_nivel():
+	var main = get_tree().current_scene
+	main.load_level(proximo_nivel)
--- Levels/niveles-finales/niveles-finales-codigo/level_3.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-@export var proximo_nivel: PackedScene #nivel cargado
-
-func _on_cambio_de_nivel_body_exited(body: Node2D) -> void:
-	if body.is_in_group("jugador") and proximo_nivel:
-		var main = get_tree().current_scene
-		main.load_level(proximo_nivel) #metodo de main para cargar proximo nivel
