25b19721a9c0cd1dd4a8e2774ea0abe29ca2fc47
Mauro Castro <maurocastruli@gmail.com>
2025-06-12 15:50:55 -0300

    arreglo

--- Code/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-@export var gravity: float = 980.0 # Fuerza de la gravedad
-
-var can_swap:bool = true
-var lauraOnTop:bool = false
-var active_player_bruno:bool = true
-var active_player_alejandra:bool = false
--- Levels/cajas-de-emociones/zona_muerte.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("jugador"):
-		print("Es personaje murio:", body)
-		get_tree().reload_current_scene()
