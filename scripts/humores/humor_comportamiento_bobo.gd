db57df6147da9f739a13e5326602ecf51b8150a8
Matías <titobasuraje@gmail.com>
2025-09-19 15:32:54 -0300

    solo debug y humo mas performante

--- scripts/humores/humor_comportamiento_bobo.gd
+++ scripts/humores/humor_comportamiento_bobo.gd
@@ -27,6 +27,7 @@ func process_physics_mechanic(personaje: PersonajePunkBase, delta: float):
 	if dist_a_chico > personaje.distancia_minima_a_chico_cuando_grande_bobo:
 		if (hay_arista_a_der and direccion_x == 1) or (hay_arista_a_izq and direccion_x == -1):
 			personaje.velocity.x = 0
+			print("Grande en ARISTA")
 		else:
 			personaje.velocity.x = direccion_x * personaje.move_speed_cuando_bobo
 			if Engine.get_physics_frames() % 120 == 0:
